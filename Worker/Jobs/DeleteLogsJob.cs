339a3835d7395d59bda55f9c01a12717b893b295
Eduardo Borsa <eborsa@loomissayles.com>
2024-01-29 12:22:26 -0300

    End of Setting Up Quartz Options
    Signed-off-by: Eduardo Borsa <eborsa@loomissayles.com>

--- Worker/Jobs/DeleteLogsJob.cs
+++ Worker/Jobs/DeleteLogsJob.cs
@@ -9,21 +9,22 @@ using Worker.Options;
 
 namespace Worker.Jobs
 {
+    [DisallowConcurrentExecution]
     public class DeleteLogsJob : IJob
     {
         private readonly ILogRepository _logRepository;
-        
-
+        private readonly int _amountOfDays;
 
         public DeleteLogsJob(ILogRepository logRepository, IOptions<DeleteLogsJobOptions> options)
         {
             _logRepository = logRepository;
-            _amountOfDays = options.Value.DeleteAfterDate ??
-                throw new ArgumentException("DeleteAfterDate was not found in configuration");
+            _amountOfDays = options.Value.AmountOfDays ??
+                throw new ArgumentException("Amount of days can't be null");
         }
         public async Task Execute(IJobExecutionContext context)
         {
-            throw new NotImplementedException();
+            var date = DateTime.Now.AddDays(-_amountOfDays);
+            await _logRepository.RemoveLogsAfterPeriodOfTime(date);
         }
     }
 }
--- Worker/Program.cs
+++ Worker/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
+using Quartz;
 using Worker.Contracts.Respositories;
+using Worker.Jobs;
+using Worker.Options;
 using Worker.Persistence;
 using Worker.Persistence.Repositories;
 
@@ -10,14 +13,29 @@ var host = Host.CreateDefaultBuilder(args)
         {
             options.UseSqlServer(hostContext.Configuration.GetConnectionString("DbConnectionString"));
         });
+
+        services.AddQuartz(q =>
+        {
+            var jobKey = new JobKey("DeleteLogsJob");
+
+            q.AddJob<DeleteLogsJob>(opts => opts.WithIdentity(jobKey));
+
+            q.AddTrigger(opts => opts
+            .ForJob(jobKey)
+            .WithIdentity($"{jobKey}-trigger")
+            .WithCronSchedule(hostContext.Configuration.GetSection("DeleteLogsJob:CronSchedule").Value ?? "0/5 * * * * ?"));
+        });
+
         services.AddScoped<ILogRepository, LogRepository>();
+        services.Configure<DeleteLogsJobOptions>(hostContext.Configuration.GetSection(DeleteLogsJobOptions.DeleteLogsJob));
     })
     .Build();
 
 using var scope = host.Services.CreateScope();
 var dbContext = scope.ServiceProvider.GetService<WorkerDbContext>();
 
-if (dbContext != null) {
+if (dbContext != null)
+{
     Seeder.Seed(dbContext);
 }
 
